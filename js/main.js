ea01862b5db8deb4a3cec040e839292bfacf4c9a
Maxim <max1m_ka@ukr.net>
2017-11-22 01:06:37 +0200

    start project

--- js/main.js
+++ js/main.js
@@ -4,14 +4,36 @@ $(document).ready(function() {
     //$(document).foundation();
 
 
-    $('.menu-open').click(function () {
+    /*$('.menu-open').click(function () {
         $('.menu-list').slideToggle(300);
     });
 
     $(window).resize(function () {
+        console.log('test');
         if ($(window).width < 768 ){
             $('.menu-list').removeAttr("style");
+            console.log('test2');
         }
-    })
+    })*/
+    $('.menu-open').click(function () {
+        $('.menu-list').slideToggle(300);
+        if ( $('.menu-open .fa').hasClass('fa-bars')){
+            $('.fa').removeClass('fa-bars');
+            $('.fa').addClass('fa-times');
+        } else if ($('.menu-open .fa').hasClass('fa-times')){
+            $('.fa').removeClass('fa-times');
+            $('.fa').addClass('fa-bars');
+        }
+    });
+    function checkWidth() {
+        var $window = $(window);
+        var windowsize = $window.width();
+        if (windowsize > 768) {
+            $('.menu-list').removeAttr("style");
+            console.log('test2')
+        }
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
 
 });
